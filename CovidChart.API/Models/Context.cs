2992bcde568877ede7db370294c8e0dedfdc6d39
ilhan enes daniş <danisenes3@gmail.com>
2022-07-19 21:31:13 +0300

    Add project files.

---
+++ CovidChart.API/Models/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CovidChart.API.Models
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options):base(options)
+        {
+        }
+        public DbSet<Covid> Covids { get; set; }
+    }
+}
